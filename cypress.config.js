6bc65cf37d6b02b4442bb088867d6f2c520d99b9
vidhyapasupathy <vidhyapasupathy@gmail.com>
2023-08-02 17:01:28 +1000

    changed folder structure

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+      support: 'e2e/support/e2e.js'// Update this with the support file path
+    }
   },
 });
